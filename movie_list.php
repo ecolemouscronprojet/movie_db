8b74280cfef32864f591b2ccbff28359c340302e
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-06-08 19:42:49 +0200

    wip

--- movie_list.php
+++ movie_list.php
@@ -1,48 +1,83 @@
 <?php
 
 require_once('connexion.php');
+$search = $_GET['search'] ?? null;
+if ($search) {
+    $query = $db->query('
+    select * from (
+    select m.id, m.name, m.release, m.duration, GROUP_CONCAT(c.name SEPARATOR \', \') as categories from movie as m
+    left join category_movie as cm on cm.movie_id = m.id
+    left join category as c on c.id = cm.category_id
+    group by id) as t where name like "%'.$search.'%" or categories like "%'.$search.'%"
+    ');
+} else {
+    $query = $db->query('select m.id, m.name, m.release, m.duration, GROUP_CONCAT(c.name SEPARATOR \', \') as categories from movie as m
+    left join category_movie as cm on cm.movie_id = m.id
+    left join category as c on c.id = cm.category_id
+    group by id');
+}
 
-$query = $db->query('select * from movie');
+
+
 
 $movies = $query->fetchAll(PDO::FETCH_ASSOC);
-
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
+
 <body>
     <div class="container-fluid">
-        <a href="movie_edit.php" class="btn btn-success m-3">Ajouter</a>
+
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <form method="get" action="">
+                    <div class="input-group mt-3">
+                        <span class="input-group-text" id="search"><i class="bi bi-search"></i></span>
+                        <input type="text" class="form-control" placeholder="Search" name="search" id="search">
+                    </div>
+                </form>
+            </div>
+            <div class="p-2">
+                <a href="movie_edit.php" class="btn btn-success m-3">Ajouter</a>
+            </div>
+        </div>
+
+
         <table class="table table-dark">
             <tr>
                 <th>#</th>
                 <th>Name</th>
+                <th>Categories</th>
                 <th>Release</th>
                 <th>Duration</th>
                 <th>ACTION</th>
             </tr>
-        <?php foreach($movies as $movie): ?>
-            <tr>
-                <td><?= $movie['id'] ?></td>
-                <td><?= $movie['name'] ?></td>
-                <td><?= $movie['release'] ?></td>
-                <td><?= $movie['duration'] ?></td>
-                <td>
-                    <a onclick="return confirm('Voulez-vous vraiment supprimer cet élement ?')" 
-                    href="movie_remove.php?id=<?= $movie['id'] ?>" class="btn btn-danger">SUPPRIMER</a>
-                    <a href="movie_edit.php?id=<?= $movie['id'] ?>" class="btn btn-primary">MODIFIER</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
+            <?php foreach ($movies as $movie) : ?>
+                <tr>
+                    <td><?= $movie['id'] ?></td>
+                    <td><?= $movie['name'] ?></td>
+                    <td><?= $movie['categories'] ?></td>
+                    <td><?= $movie['release'] ?></td>
+                    <td><?= $movie['duration'] ?></td>
+                    <td>
+                        <a onclick="return confirm('Voulez-vous vraiment supprimer cet élement ?')" href="movie_remove.php?id=<?= $movie['id'] ?>" class="btn btn-danger">SUPPRIMER</a>
+                        <a href="movie_edit.php?id=<?= $movie['id'] ?>" class="btn btn-primary">MODIFIER</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
 
         </table>
     </div>
 </body>
+
 </html>
